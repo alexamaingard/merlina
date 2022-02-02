9a71f27b09f3cde9a1d261162fa3054efeb502dc
lexymcr <@maingard.alexa@gmail.com>
2022-02-02 18:53:56 +0100

    Base Design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { WebsiteAlexa } from "./components/WebsiteAlexa";
 import "./styles/App.css"
+import "./styles/reset.css"
 
 export const App = () => {
   return (
     <div className="App">
+      <WebsiteAlexa />
     </div>
   );
 }
