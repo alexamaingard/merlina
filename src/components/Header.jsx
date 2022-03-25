6e81599d1bc966514e043f2ce6c9560269ecb3d6
alexamaingard <maingard.alexa@gmail.com>
2022-03-25 19:43:51 +0100

    add placeholder to redirect to profile page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,20 @@ import '../styles/header.css'
 export const Header = () => {
     return(
         <nav className="header">
+            <div id='header-left'>
+                <a href='#profile'>Placeholder</a>
+            </div>
             <ul>
                 <li>
-                    <a href="#about-me">About Me</a>
+                    <a href='#about-me'>About Me</a>
                 </li>
                 <li>
-                    <a href="#portfolio">Portfolio</a>
+                    <a href='#portfolio'>Portfolio</a>
                 </li>
                 <li>
-                    <a href="#contact">Contact</a>
+                    <a href='#contact'>Contact</a>
                 </li>
-                <li id="resume-button">Resume</li>
+                <li id='resume-button'>Resume</li>
             </ul>
         </nav>
     )
