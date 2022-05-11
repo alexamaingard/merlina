89faadeba8708339d0ab0e5a594026a55049ddba
alexamaingard <maingard.alexa@gmail.com>
2022-05-11 09:41:24 +0200

    add resume link to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return(
         <nav className="header">
             <div id='header-left'>
-                <a href='#profile'>Placeholder</a>
+                <a href='#profile'>AM</a>
             </div>
             <ul>
                 <li>
@@ -16,7 +16,15 @@ export const Header = () => {
                 <li>
                     <a href='#contact'>Contact</a>
                 </li>
-                <li id='resume-button'>Resume</li>
+                <li id='resume-button'>
+                    <a 
+                        href='https://www.canva.com/design/DAE9kmsCHXE/5Dvu7wlsB-aU-YHn00Kf4A/view#1'
+                        target='_blank'
+                        rel="noreferrer"    
+                    >
+                        Resume
+                    </a>
+                </li>
             </ul>
         </nav>
     )
